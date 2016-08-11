e79315cc7a8934324889ba63fe6ec260420e30e1
Xmal Morthen <xmal.morthen@gmail.com>
2016-08-11 18:41:36 -0500

    Signed-off-by: Xmal Morthen <xmal.morthen@gmail.com>

--- Build/Build CELLTRACKER.bat
+++ Build/Build CELLTRACKER.bat
@@ -4,4 +4,5 @@ mkdir .\%builddir%
 del /q /s .\%builddir%\*.*
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /fl /flp:LogFile="..\Build\%builddir%\buildLauncher.log";Append;errorsonly /nologo /p:WarningLevel=0;Optimize=True;OutputPath="..\Build\%builddir%";Platform=AnyCPU;TargetFrameworkVersion=v4.5 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\CellTrackLauncher\CellTrackLauncher.csproj
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /fl /flp:LogFile="..\Build\%builddir%\buildCelltrack.log";Append;errorsonly /nologo /p:WarningLevel=0;Optimize=True;OutputPath="..\Build\%builddir%";Platform=AnyCPU;TargetFrameworkVersion=v4.5 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\CellTrack\CellTrack.csproj
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /fl /flp:LogFile="..\Build\%builddir%\ShutDownServer.log";Append;errorsonly /nologo /p:WarningLevel=0;Optimize=True;OutputPath="..\Build\%builddir%\SHUTDOWSERVER";Platform=AnyCPU;TargetFrameworkVersion=v4.5 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\ShutDownServer\ShutDownServer.csproj
 pause
